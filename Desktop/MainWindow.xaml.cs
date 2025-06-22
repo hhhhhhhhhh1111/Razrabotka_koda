f65fa96cb48a6aa8419018c2d00452c263e86d31
MDirovvvv <mishkadirovv2.0@gmail.com>
2025-06-22 21:40:29 +0300

    lab3

---
+++ Desktop/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Desktop.Repository;
+using Desktop.Utiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+
+            window1.Show();
+
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string email = textBox.Text; // Email
+            string password = textBox1.Text; // Пароль
+
+            if (!email.ValidateEmail())
+            {
+                MessageBox.Show("Введите корректную почту.");
+                return;
+            }
+
+            if (!password.ValidatePassword())
+            {
+                MessageBox.Show("Пароль должен быть не менее 6 символов.");
+                return;
+            }
+            string errorMessage;
+            bool isRegistered = UserRepository.AuthenticateUser(email, password, out errorMessage);
+
+            if (!isRegistered)
+            {
+                MessageBox.Show(errorMessage);
+            }
+            else
+            {
+                // Данные корректны
+                MessageBox.Show("Вход успешно выполнен!");
+
+                Main_empty main_empty = new Main_empty();
+
+                main_empty.Show();
+
+                this.Close();
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark();
+        }
+
+        private void UpdateWatermark()
+        {
+            watermark.Visibility = string.IsNullOrWhiteSpace(textBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void TextBox_TextChanged1(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark1();
+        }
+        private void UpdateWatermark1()
+        {
+            watermark1.Visibility = string.IsNullOrWhiteSpace(textBox1.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
+
